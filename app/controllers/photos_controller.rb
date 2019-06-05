1f098b95447b0dc1e3330f460f1cfe8cbae7234f
Majun1990 <merijun_a@hotmail.com>
2019-06-05 19:12:12 +0000

    button still not working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,10 @@
 class PhotosController < ApplicationController
 before_action :authenticate_user!
+
   def create
-    @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
-    redirect_to place_path(@place)
+    @photo = Photo.find(params[:place_id])
+    @photo.create(photo_params.merge(user: current_user))
+
   end
 
   private
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  has_many :photos
   mount_uploader :picture, PictureUploader
+  serialize :picture, JSON
 end
